a817f59b2112ad2441887ec23061cb1026fc7695
Nycolle <teemoistnicht@gmail.com>
2024-11-27 21:54:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById("dropdown-menu");
+    dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
+  }
+  
